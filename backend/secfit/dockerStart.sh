538afe99b5b09931b45749f79b2d71265f0644a4
Jone Vassbø <jone.vassbo@gmail.com>
2021-03-06 11:15:05 +0100

    Added replicas:2 and better naming

--- backend/secfit/dockerStart.sh
+++ backend/secfit/dockerStart.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# start.sh
+# dockerStart.sh
+# This file runs at container startup
 python manage.py migrate
 
 gunicorn secfit.wsgi --log-file - -b 0.0.0.0:8000
--- frontend/dockerStart.sh
+++ frontend/dockerStart.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# start.sh
+# dockerStart.sh
+# This file runs at container startup
 echo "const HOST = '${HOST}';" > ./www/scripts/defaults.js
 
 cordova run browser --release --port=3000

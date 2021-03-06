7cef13e5d9b4f5e55a438894d516e5fc00038d02
Jone Vassbø <jone.vassbo@gmail.com>
2021-03-06 18:08:03 +0100

    Initial setup

--- backend/secfit/users/tests.py
+++ backend/secfit/users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class UserSerializerTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        print("Test for UserSerializer")
+        self.assertTrue(True)
--- backend/secfit/workouts/tests.py
+++ backend/secfit/workouts/tests.py
@@ -4,3 +4,11 @@ Tests for the workouts application.
 from django.test import TestCase
 
 # Create your tests here.
+
+class WorkoutPermissionsTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        print("Test for WorkoutPermissions")
+        self.assertTrue(True)

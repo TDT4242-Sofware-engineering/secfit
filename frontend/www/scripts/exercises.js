e7f7784512b8b67cba034b9bbcbb973bcf2f29db
Mathias Lund Ahrn <golvkaldt@gmail.com>
2021-04-18 21:34:41 +0200

    Prettified javascript files

--- frontend/www/scripts/exercises.js
+++ frontend/www/scripts/exercises.js
@@ -1,42 +1,44 @@
 async function fetchExerciseTypes(request) {
-    let response = await sendRequest("GET", `${HOST}/api/exercises/`);
+  let response = await sendRequest("GET", `${HOST}/api/exercises/`);
 
-    if (response.ok) {
-        let data = await response.json();
+  if (response.ok) {
+    let data = await response.json();
 
-        let exercises = data.results;
-        let container = document.getElementById('div-content');
-        let exerciseTemplate = document.querySelector("#template-exercise");
-        exercises.forEach(exercise => {
-            const exerciseAnchor = exerciseTemplate.content.firstElementChild.cloneNode(true);
-            exerciseAnchor.href = `exercise.html?id=${exercise.id}`;
+    let exercises = data.results;
+    let container = document.getElementById("div-content");
+    let exerciseTemplate = document.querySelector("#template-exercise");
+    exercises.forEach((exercise) => {
+      const exerciseAnchor = exerciseTemplate.content.firstElementChild.cloneNode(
+        true
+      );
+      exerciseAnchor.href = `exercise.html?id=${exercise.id}`;
 
-            const h5 = exerciseAnchor.querySelector("h5");
-            h5.textContent = exercise.name;
+      const h5 = exerciseAnchor.querySelector("h5");
+      h5.textContent = exercise.name;
 
-            const p = exerciseAnchor.querySelector("p");
-            p.textContent = exercise.description;   
+      const p = exerciseAnchor.querySelector("p");
+      p.textContent = exercise.description;
 
-            container.appendChild(exerciseAnchor);
-        });
-    }
+      container.appendChild(exerciseAnchor);
+    });
+  }
 
-    return response;
+  return response;
 }
 
 function createExercise() {
-    window.location.replace("exercise.html");
+  window.location.replace("exercise.html");
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
-    let createButton = document.querySelector("#btn-create-exercise");
-    createButton.addEventListener("click", createExercise);
+  let createButton = document.querySelector("#btn-create-exercise");
+  createButton.addEventListener("click", createExercise);
 
-    let response = await fetchExerciseTypes();
-    
-    if (!response.ok) {
-        let data = await response.json();
-        let alert = createAlert("Could not retrieve exercise types!", data);
-        document.body.prepend(alert);
-    }
+  let response = await fetchExerciseTypes();
+
+  if (!response.ok) {
+    let data = await response.json();
+    let alert = createAlert("Could not retrieve exercise types!", data);
+    document.body.prepend(alert);
+  }
 });
